e252b8c0ca453d1aa3d8b48664b77c936f10bc87
David Roberts <davidroberts@davids-mac-pro.home>
2016-11-06 21:28:07 +0000

    Some more tests

--- src/vector.nim
+++ src/vector.nim
@@ -1,3 +1,5 @@
+import math
+
 type
   Vector* = ref object of RootObj
     x*, y*, z*: float32
@@ -8,8 +10,48 @@ proc newVector*(x, y, z: float32): Vector =
   result.y = y
   result.z = z
 
+proc p_norm*(v: Vector, p: float32): float32 =
+  let xx = pow(v.x, p)
+  let yy = pow(v.y, p)
+  let zz = pow(v.z, p)
+
+  let sqr = xx+yy+zz
+  result = pow(sqr, 1.0/p)
+
+proc magnitude*(v: Vector): float32 =
+  result = v.p_norm(2)
+
 proc `+`*(a, b: Vector): Vector =
   result = newVector(a.x+b.x, a.y+b.y, a.z+b.z)
 
+proc `+=`*(v: var Vector, other: Vector) =
+  v.x += other.x
+  v.y += other.y
+  v.z += other.z
+
+proc `-`*(a, b: Vector): Vector =
+  result = newVector(a.x-b.x, a.y-b.y, a.z-b.z)
+
+proc `-=`*(v: var Vector, other: Vector) =
+  v.x -= other.x
+  v.y -= other.y
+  v.z -= other.z
+
 proc `==`*(a, b: Vector): bool =
   result = (a.x == b.x) and (a.y == b.y) and (a.z == b.z)
+
+proc `*`*(v: Vector, s: float32): Vector =
+  result = newVector(v.x*s, v.y*s, v.z*s)
+
+proc `*=`*(v: var Vector, s: float32) =
+  v.x *= s
+  v.y *= s
+  v.z *= s
+
+proc `/`*(v: Vector, s: float32): Vector =
+  result = newVector(v.x/s, v.y/s, v.z/s)
+
+proc `/=`*(v: var Vector, s: float32) =
+  v.x /= s
+  v.y /= s
+  v.z /= s
--- tests/vector_test.nim
+++ tests/vector_test.nim
@@ -9,8 +9,47 @@ suite "vector suite":
     check res == true
 
   test "addition":
-      let a = newVector(10, 20, 30)
-      let b = newVector(30, 40, 50)
-      let res = a+b
-      let exp = newVector(40, 60, 80)
-      check exp == res
+    let a = newVector(10, 20, 30)
+    let b = newVector(30, 40, 50)
+    let res = a+b
+    let exp = newVector(40, 60, 80)
+    check exp == res
+
+  test "additionInPlace":
+    var a = newVector(10, 20, 30)
+    let b = newVector(30, 40, 50)
+    a+=b
+    let exp = newVector(40, 60, 80)
+    check exp == a
+
+  test "subtraction":
+    let a = newVector(10, 40, 87)
+    let b = newVector(30, 20, 50)
+    let res = a-b
+    let exp = newVector(-20, 20, 37)
+    check exp == res
+
+  test "subtractionInPlace":
+    var a = newVector(10, 40, 87)
+    let b = newVector(30, 20, 50)
+    a-=b
+    let exp = newVector(-20, 20, 37)
+    check exp == a
+
+  test "multScalar":
+    let a = newVector(10, 20, 30)
+    let res = a*8
+    let exp = newVector(80, 160, 240)
+    check exp == res
+
+  test "divScalar":
+    let a = newVector(10, 20, 30)
+    let res = a/5
+    let exp = newVector(2, 4, 6)
+    check exp == res
+
+  test "magnitude":
+    let a = newVector(5, 10, 10)
+    let res = a.magnitude()
+    let exp = 15
+    check exp == res
